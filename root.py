8061dffb3230a71599d1d43db0b5265703808029
TD21forever <435212619@qq.com>
2023-10-01 19:25:59 +0800

    100 feat: 增加了yaml到opml的转换

--- root.py
+++ root.py
@@ -5,8 +5,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 absolute = partial(os.path.join, BASE_DIR)
 
 LOG_DIR = absolute("log")
-DOCS_DIR = absolute("doc")
+DOCS_DIR = absolute("docs")
 
 RSS_TEMPLATE_PATH = absolute("resource/template.xml")
 RSS_HTML_TEMPLATE_PATH = absolute("resource/template.html")
 CONFIG_PATH = absolute("resource/config.yml")
+
+
+BASE_URL = "https://dcts.top/docsrss/"
